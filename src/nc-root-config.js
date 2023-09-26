a7842be87b2952bae1cd3ec67676b219f943c8ab
nogueicb <Caio.Nogueira@btgpactual.com>
2023-09-26 17:32:10 -0300

    adicionando dashboard e register

--- src/nc-root-config.js
+++ src/nc-root-config.js
@@ -1,11 +1,20 @@
 import { registerApplication, start } from "single-spa";
 
 registerApplication({
+  name: "@nc/dashboard",
+  app: () => System.import("@nc/dashboard"),
+  activeWhen: ["/dashboard"],
+});
+registerApplication({
+  name: "@nc/register",
+  app: () => System.import("@nc/register"),
+  activeWhen: ["/register"],
+});
+registerApplication({
   name: "@nc/menu",
   app: () => System.import("@nc/menu"),
   activeWhen: ["/"],
 });
-
 start({
   urlRerouteOnly: true,
 });
